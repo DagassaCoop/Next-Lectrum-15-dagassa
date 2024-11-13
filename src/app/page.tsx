0c3b983efda1b1e82501d85ea6d3e99d9c50ca1c
dagas_sa <dagassacoop@gmail.com>
2024-11-13 16:05:49 -0500

    task complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,20 @@
-import Link from 'next/link'
+import { cookies } from "next/headers";
 
-const LINKS: {
-  title: string
-  path: string
-}[] = [
-  {
-    title: "About",
-    path: "/about"
-  },
-  {
-    title: "Profile",
-    path: "/profile"
-  }
-]
+export default async function Home() {
+  const cookieStore = await cookies();
+  const isUserGuest = cookieStore.get("isUserGuest");
 
-export default function Home() {
+  console.log(isUserGuest);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <header className='flex items-center justify-between w-1/6 mx-auto'>
-        {LINKS.map((l, index) => {
-          return <Link href={l.path} key={index}>{l.title}</Link>
-        })}
-      </header>
-        
+      <main>
+        {isUserGuest?.value !== "false" ? (
+          <h1>Ласкаво просимо!</h1>
+        ) : (
+          <h1>Привіт, раді бачити тебе знову!</h1>
+        )}
+      </main>
     </div>
   );
 }
