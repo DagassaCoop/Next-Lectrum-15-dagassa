f07a3cec1897ef50fb61f71c493231ecded0eda4
dagas_sa <dagassacoop@gmail.com>
2024-12-11 22:38:48 -0500

    completed: tasks 1-5

--- web-app/next.config.ts
+++ web-app/next.config.ts
@@ -3,6 +3,40 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: [
+      "https://dims.apnews.com",
+      "news.vt.edu",
+      "img.ksl.com",
+      "a4.espncdn.com",
+      "www.washingtonpost.com",
+      "media.cnn.com",
+      "sportshub.cbsistatic.com",
+      "deadline.com",
+      "www.cidrap.umn.edu",
+      "gizmodo.com",
+      "static.politico.com",
+      "scpr.brightspotcdn.com",
+      "i.insider.com",
+      "media.tegna-media.com",
+      "s.yimg.com",
+      "m-cdn.phonearena.com",
+      "cdn.zmescience.com",
+      "static.files.bbci.co.uk",
+      "www.reuters.com",
+      "cdn.nba.com",
+      "s.hdnux.com",
+      "www.hollywoodreporter.com",
+      "ichef.bbci.co.uk",
+      "www.masslive.com",
+      "platform.polygon.com",
+      "images.actionnetwork.com",
+      "cdn.arstechnica.net",
+      "variety.com",
+      "dims.apnews.com",
+      "techcrunch.com",
+    ],
+  },
 };
 
 export default nextConfig;
--- web-app/src/pages/index.tsx
+++ web-app/src/pages/index.tsx
@@ -1,114 +1,123 @@
+"use client";
+
+import { GetServerSideProps } from "next";
+import { ChangeEventHandler, useState } from "react";
+
+// Entities
+import { News, NewsApiResponse, Source, SourcesApiResponse } from "@/types";
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import Link from "next/link";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+interface HomeProps {
+  sources: Source[];
+  news: News[];
+}
+const API = "1783adec1406491fbce92d4b0d9411da";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+export default function Home({ news, sources }: HomeProps) {
+  const [source, setSource] = useState<string>("");
+  const [filteredNews, setFilteredNews] = useState(news);
 
-export default function Home() {
+  console.log(process.env.NEXT_PUBLIC_NEWS_API_KEY);
+
+  const handleSelect: ChangeEventHandler<HTMLSelectElement> = (e) => {
+    console.log(e.target.value);
+    setSource(e.target.value);
+    setFilteredNews(() => {
+      if (e.target.value === "") {
+        return news;
+      } else {
+        return news.filter((item) => item.source.id === e.target.value);
+      }
+    });
+  };
+
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="w-full py-6 px-10">
+      <main>
+        <div className="w-full mb-10 flex justify-center">
+          <select
+            name="sources"
+            id="source-select"
+            value={source}
+            onChange={handleSelect}
+            className="text-black py-2 px-4"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <option value="">All sources</option>
+            {sources.map((item) => {
+              return (
+                <option value={item.id} key={item.id}>
+                  {item.name}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <div className="grid grid-cols-3 gap-6">
+          {filteredNews.map((item) => {
+            return (
+              <div
+                key={item.url}
+                className="relative border border-white rounded-lg overflow-hidden flex flex-col"
+              >
+                <div className="relative w-full h-[200px] overflow-hidden mb-4">
+                  <Image
+                    src={item.urlToImage!}
+                    alt={item.title}
+                    layout="fill"
+                    objectFit="cover"
+                    className="hover:scale-110 transition ease-in-out duration-500"
+                  />
+                </div>
+                <div className="bg-white text-black p-4 flex flex-col justify-between flex-1">
+                  <div className="flex flex-col gap-4 mb-8">
+                    <h2 className="font-semibold hover:text-blue-500 transition ease-in-out">
+                      {item.title}
+                    </h2>
+                    <p>{new Date(item.publishedAt).toLocaleDateString()}</p>
+                    <p>{item.description}</p>
+                  </div>
+                  <Link href={""} className="font-bold text-lg text-blue-500">
+                    Read more
+                  </Link>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  // Sources
+  const resSources = await fetch(
+    "https://newsapi.org/v2/top-headlines/sources?country=us&" +
+      `apiKey=${process.env.NEWS_API_KEY ?? API}`
+  );
+  const sourcesAll: Source[] = ((await resSources.json()) as SourcesApiResponse)
+    .sources;
+
+  // News
+  const resNews = await fetch(
+    "https://newsapi.org/v2/top-headlines?country=us&pageSize=100&" +
+      `apiKey=${process.env.NEWS_API_KEY ?? API}`
+  );
+  const news: News[] = ((await resNews.json()) as NewsApiResponse).articles;
+
+  const uniqSourceIds = new Set(news.map((item) => item.source.id));
+
+  const sources: Source[] = [];
+  uniqSourceIds.forEach((item) => {
+    const source = sourcesAll.find((subitem) => subitem.id === item);
+    if (source) sources.push(source);
+  });
+
+  return {
+    props: {
+      sources,
+      news,
+    },
+  };
+};
