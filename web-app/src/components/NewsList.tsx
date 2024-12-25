8cf7260a160db3643fece66bd2c0226ad3a01bca
dagas_sa <dagassacoop@gmail.com>
2024-12-24 21:37:02 -0500

    WIP: error 418

--- web-app/src/components/NewsList.tsx
+++ web-app/src/components/NewsList.tsx
@@ -1,54 +1,17 @@
-"use client";
-import Image from "next/image";
-import Link from "next/link";
-
 // Entities
 import { News } from "@/types";
+// Components
+import NewsListItem from "./NewsListItem";
 
 interface NewsListProps {
   news: News[];
 }
 
-const getFormatDate = (date: string) => {
-  return new Date(date).toLocaleDateString();
-};
-
 export default function NewsList({ news }: NewsListProps) {
   return (
     <div className="grid grid-cols-3 gap-6">
-      {news.map((item) => {
-        return (
-          <div
-            key={item.url}
-            className="relative border border-white rounded-lg overflow-hidden flex flex-col"
-          >
-            <div className="relative w-full h-[200px] overflow-hidden">
-              {item.urlToImage ? (
-                <Image
-                  src={item.urlToImage}
-                  alt={item.title}
-                  fill={true}
-                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw"
-                  className="hover:scale-110 transition ease-in-out duration-500"
-                />
-              ) : (
-                <div className="w-full h-full bg-gray-400"></div>
-              )}
-            </div>
-            <div className="bg-white text-black p-4 flex flex-col justify-between flex-1">
-              <div className="flex flex-col gap-4 mb-8">
-                <h2 className="font-semibold hover:text-blue-500 transition ease-in-out">
-                  {item.title}
-                </h2>
-                <p>{getFormatDate(item.publishedAt)}</p>
-                <p>{item.description}</p>
-              </div>
-              <Link href={""} className="font-bold text-lg text-blue-500">
-                Read more
-              </Link>
-            </div>
-          </div>
-        );
+      {news.map((item, index) => {
+        return <NewsListItem news={item} key={index} />;
       })}
     </div>
   );
